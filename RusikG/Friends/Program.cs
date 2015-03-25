adf4d7a4eed643c458fed3ab1b5cc9d1e6c1de9c
RusikG <keh.ruslan@gmail.com>
2015-03-26 00:15:31 +0200

    Fix

--- RusikG/Friends/Program.cs
+++ RusikG/Friends/Program.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Friends
 {
