6b3d98268dbb57832be3ce9807f768c3d7a573e0
woody01 <vadimosipov24@gmail.com>
2015-03-28 10:30:36 +0200

    Fix #30 issue

--- woody01/Budget.cpp
+++ woody01/Budget.cpp
@@ -12,27 +12,26 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	cin >> filename >> budget;
 	ifstream input(filename + ".tsv");
-
-	string regionname[1000];
-	int population[1000];
+	vector<string> regionname;
+	vector<int> population;
 	int i = 0;
 
 	while (!input.eof()){
-	input >> regionname[i] >> population[i];
-	i=i+1;
+		input >> regionname[i] >> population[i];
+		i=i+1;
 	}
 	int n = i;
 	int sum = 0;
 
 	for (int i = 0; i < n; i++){
-	sum = sum + population[i];
+		sum = sum + population[i];
 	}
 
 	cout << budget / sum <<"\n" ;
 
 	ofstream output("output.tsv");
 	for (int i = 0; i < n; i++){
-	output << regionname[i] << "	" << (population[i] * budget / sum) << "\n";
+		output << regionname[i] << "	" << (population[i] * budget / sum) << "\n";
 	}
 
 	input.close();
