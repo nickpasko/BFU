5433d780b873a9d21619fcf36462b22299399662
Autumn Cat <whitedragon3907@gmail.com>
2015-04-14 00:23:57 +0200

    Update Program.cs

--- TheLastSoulTiger/Program.cs
+++ TheLastSoulTiger/Program.cs
@@ -8,24 +8,25 @@ namespace Random1
 {
     class Program
     { //придумать ввод и передачу значений через arqs 
-        static void Main(string[] args)
+        static void Main(string (string (Char[]argsmale); string (Char[] argsfemale)) // Не знаю синтаксис
         {
+            for (int i = 0; i <= 10; ++i)
             Random rnd = new Random();
-            string[] maleNames = { "Rufus", "Bear", "Dakota", "Fido", 
+            Char argsmale = new argsmale { "Rufus", "Bear", "Dakota", "Fido", 
                                 "Vanya", "Samuel", "Koani", "Volodya", 
-                                "Prince", "Yiska" };
-            string[] femaleNames = { "Maggie", "Penny", "Saya", "Princess", 
+                                "Prince", "Yiska" }; // поле, но используется как тип
+            Char argsfema = new argsfemale { "Maggie", "Penny", "Saya", "Princess", 
                                   "Abby", "Laila", "Sadie", "Olivia", 
                                   "Starlight", "Talla" };
 
             // Generate random indexes for names 
-            int mIndex = rnd.Next(maleNames.Length);
-            int fIndex = rnd.Next(femaleNames.Length);
+            char mIndex = rnd.Next(maleNames.Length); // ошибка, что-то другое должно быть?
+            char fIndex = rnd.Next(femaleNames.Length);
 
             // Display the result 
-            Console.WriteLine("{0}\nlove", maleNames[mIndex]);
+            Console.Line("{0}\nlove", maleNames[mIndex]);
             //Console.WriteLine("love");
-            Console.WriteLine("{0}", femaleNames[fIndex]);
+            Console.Line("{0}", femaleNames[fIndex]);
         }
     }
 }
